a5427651e8cd31a513305a09f59d6b67e269c837
kevs01 <kevrs0311@gmail.com>
2023-11-22 01:43:19 -0600

    shells of course unit tests

--- Unit Tests/courseunittest.py
+++ Unit Tests/courseunittest.py
@@ -1,18 +1,39 @@
 import sys
 
 sys.path.append('../SchedulingApp')
-from SchedulingApp.models import  MyUser, Course, Section
-from SchedulingApp.functions import func_ValidateCourseName, func_ValidateDepartment, func_ValidateCourseNumber, func_ValidateYear
+from SchedulingApp.models import MyUser, Course, Section
+from SchedulingApp.functions import func_ValidateCourseName, func_ValidateDepartment, func_ValidateCourseNumber, \
+    func_ValidateYear
 from django.test import TestCase, Client
 
-class ValidateCourseNameTest(TestCase):
-    pass
 
-class ValidateDepartment(TestCase):
-    pass
+class ValidateCourseNameTest(TestCase):
+    def testValidCourseName(self):
+        pass
+
+    def testInvalidCourseName(self):
+        pass
+
+
+class ValidateDepartmentTest(TestCase):
+    def testValidDepartment(self):
+        pass
+
+    def testInvalidDepartment(self):
+        pass
+
 
 class ValidateCourseNumberTest(TestCase):
-    pass
+    def testValidCourseNumber(self):
+        pass
+
+    def testInvalidCourseNumber(self):
+        pass
+
 
 class ValidateYearTest(TestCase):
-    pass
+    def testValidYear(self):
+        pass
+
+    def testInvalidYear(self):
+        pass
