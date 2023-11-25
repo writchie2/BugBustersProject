33fa122474d5da2700be149145d9e973dd251ebb
nchaimlo <chaimlo2@uwm.edu>
2023-11-24 21:11:33 -0600

    Completed tests on number validation

--- UnitTests/myuserunittest.py
+++ UnitTests/myuserunittest.py
@@ -13,7 +13,8 @@ class ValidateEmailTest(TestCase):
     min_email_length = 9
 
     invalid_emails = ["invalid_email", "user@edu", "user@.edu", "a",
-                      "user@uwm.com ", "user.edu", "@uwm.edu"]
+                      "user@uwm.com ", "user.edu", "@uwm.edu",
+                      "person 1@uwm.edu", ".person@uwm.edu", "per..son@uwm.edu"]
 
     invalid_whitespace = [" user@uwm.edu", "user@uwm.edu ", " user@uwm.edu "]
 
@@ -58,6 +59,7 @@ class ValidateFirstNameTest(TestCase):
     long_name = "A" * 21
     valid_names = ["Adam", "Penelope", "Riley", "Jackson", "Aaaaaaaaaaaaaaaaaaaa",
                    "McDonald", "Edwards"]
+
     def test_no_name(self):
         with self.assertRaises(ValueError):
             func_ValidateFirstName("")
@@ -117,6 +119,43 @@ class ValidateLastNameTest(TestCase):
 class ValidatePhoneNumberTest(TestCase):
     valid_phone_numbers = ["414-123-4567", "(414)123-4567",
                            "4141234567", "(414)1234567"]
+
+    invalid_phone_numbers = ["414-123-456", "123-4567",
+                             "1", "(414)", "1234",
+                             "(414)12E-4567", "414-123-$567",
+                             "414 123 4567"]
+
+    invalid_whitespace = [" (414)123-4567", "(414)123-4567 ", " (414)123-4567 "]
+
+    long_number = 123456789012345678901
+
+    def test_no_number(self):
+        with self.assertRaises(ValueError):
+            func_ValidatePhoneNumber("")
+
+    def test_empty_number(self):
+        with self.assertRaises(ValueError):
+            func_ValidatePhoneNumber(" ")
+
+    def test_valid_numbers(self):
+        try:
+            for number in self.valid_phone_numbers:
+                func_ValidatePhoneNumber(number)
+        except Exception as e:
+            self.fail(f"func_ValidatePhoneNumber raised an exception: {e}")
+
+    def test_invalid_numbers(self):
+        for number in self.invalid_phone_numbers:
+            with self.assertRaises(ValueError):
+                func_ValidatePhoneNumber(number)
+
+    def test_long_number(self):
+        with self.assertRaises(ValueError):
+            func_ValidatePhoneNumber(self.long_number)
+
+    def test_whitespace(self):
+        for number in self.invalid_whitespace:
+            func_ValidatePhoneNumber(number)
 
 
 class ValidateStreetAddressTest(TestCase):
