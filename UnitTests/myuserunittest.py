cfad782b82efd241c14613d56f889539f70c5858
nchaimlo <chaimlo2@uwm.edu>
2023-11-24 22:45:54 -0600

    Completed tests on street address, city, state, zip

--- UnitTests/myuserunittest.py
+++ UnitTests/myuserunittest.py
@@ -33,7 +33,7 @@ class ValidateEmailTest(TestCase):
         try:
             func_ValidateEmail(email)
         except Exception as e:
-            self.fail(f"func_Validate_email raised an exception: {e}")
+            self.fail(f"func_ValidateEmail raised an exception: {e}")
 
     def test_invalid_formats(self):
         for email in self.invalid_emails:
@@ -159,16 +159,126 @@ class ValidatePhoneNumberTest(TestCase):
 
 
 class ValidateStreetAddressTest(TestCase):
-    pass
+    valid_street = ["1234 South Main Street", "1234 E Main St",
+                    "1234 W Main Street", "1234 North Main St",
+                    "1234 Main Street", "1234 Main St",
+                    "1234 South Rio Grande Avenue"]
+
+    invalid_street = ["1st St", "South University st",
+                      "1234 Avenue", "12 34 W Kenwood Blvd",
+                      "5678 South", "North Ave",
+                      "!2E4 East Washington Ave"]
+
+    long_street = "1234567890 Soooooouth Rioooooooo Saladooooo Roooooooad"
+
+    def test_no_address(self):
+        func_ValidateStreetAddress("")
+
+    def test_empty_address(self):
+        func_ValidateStreetAddress(" ")
+
+    def test_valid_address(self):
+        try:
+            for address in self.valid_street:
+                func_ValidateStreetAddress(address)
+        except Exception as e:
+            self.fail(f"func_ValidatePhoneNumber raised an exception: {e}")
+
+    def test_invalid_address(self):
+        for address in self.invalid_street:
+            with self.assertRaises(ValueError):
+                func_ValidateStreetAddress(address)
+
+    def test_long_address(self):
+        with self.assertRaises(ValueError):
+            func_ValidateStreetAddress(self.long_street)
 
 
 class ValidateCityTest(TestCase):
-    pass
+    valid_city = ["Milwaukee", "Los Angeles", "Cleveland", "Orlando",
+                  "Houston", "Seattle", "San Juan", "Chicago", "Mundelein",
+                  "Miami", "San Francisco", "Phoenix", "Ponce de Leon",
+                  "San Diego", "Puerto Penasco"]
+
+    invalid_city = ["a", "L0s Ang3l3s", "Ch!c@go"]
+
+    long_city = "A" * 20
+
+    def test_no_city(self):
+        func_ValidateCity("")
+
+    def test_empty_city(self):
+        func_ValidateCity(" ")
+
+    def test_valid_city(self):
+        try:
+            for city in self.valid_city:
+                func_ValidateCity(city)
+        except Exception as e:
+            self.fail(f"func_ValidateCity raised an exception: {e}")
+
+    def test_invalid_city(self):
+        for city in self.invalid_city:
+            with self.assertRaises(ValueError):
+                func_ValidateCity(city)
+
+    def test_long_city(self):
+        with self.assertRaises(ValueError):
+            func_ValidateCity(self.long_city)
 
 
 class ValidateStateTest(TestCase):
-    pass
+    valid_state = ["AL", "AK", "AZ", "AR",
+                   "CA","CO", "CT","DC",
+                   "DE", "FL", "GA","HI",
+                   "ID", "IL","IN", "IA",
+                   "KS", "KY", "LA", "ME",
+                   "MD", "MA", "MI", "MN",
+                   "MS", "MO", "MT", "NE",
+                   "NV", "NH", "NJ", "NM",
+                   "NY", "NC", "ND", "OH",
+                   "OK", "OR", "PA", "RI",
+                   "SC", "SD", "TN", "TX",
+                   "UT", "VT", "VA", "WA",
+                   "WV", "WI", "WY"]
 
+    invalid_state = ["Wisconsin", "Wi", "i",
+                     "IDWICA"]
+
+    invalid_whitespace = [" WI", "WI ", " WI "]
+
+    def test_no_state(self):
+        func_ValidateState("")
+
+    def test_empty_state(self):
+        func_ValidateState(" ")
+
+    def test_valid_state(self):
+        try:
+            for state in self.valid_state:
+                func_ValidateState(state)
+        except Exception as e:
+            self.fail(f"func_ValidateState raised an exception: {e}")
+
+    def test_invalid_state(self):
+        for state in self.invalid_state:
+            with self.assertRaises(ValueError):
+                func_ValidateState(state)
 
 class ValidateZipCodeTest(TestCase):
-    pass
+    valid_zip = [53206, 12345, 53220]
+    invalid_zip = [53, 2, -53220]
+    def test_no_zip(self):
+        func_ValidateZipCode(0)
+
+    def test_valid_zip(self):
+        try:
+            for zip_code in self.valid_zip:
+                func_ValidateZipCode(zip_code)
+        except Exception as e:
+            self.fail(f"func_ValidateZipCode raised an exception: {e}")
+
+    def test_invalid_zip(self):
+        for zip_code in self.invalid_zip:
+            with self.assertRaises(ValueError):
+                func_ValidateState(zip_code)
